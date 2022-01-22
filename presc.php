73a69d87ab23c47054731c123110eb6bdfaefdb1
rwema3 <rwemadominik3@gmail.com>
2022-01-22 19:54:51 +0200

    Hospital system update
    Hospital system update

--- presc.php
+++ presc.php
@@ -26,9 +26,9 @@ if(!$_SESSION['valid_user']){
 body{width:610px;}
 .frmSearch {border: 1px solid #a8d4b1;background-color: #c6f7d0;margin: 2px 0px;padding:40px;border-radius:4px;}
 #country-list{float:left;list-style:none;margin-top:-3px;padding:0;width:550px;position: absolute;}
-#country-list li{padding: 10px; background: #f0f0f0; border-bottom: #bbb9b9 1px solid;}
+#country-list li{padding: 11px; background: #f0f0f0; border-bottom: #bbb9b9 1px solid;}
 #country-list li:hover{background:#ece3d2;cursor: pointer;}
-#search-box{padding: 10px;border: #a8d4b1 1px solid;border-radius:4px;}
+#search-box{padding: 10px;border: #a8d4b1 2px solid;border-radius:4px;}
 .button {border:hidden;display: inline-block; border-radius: 4px;background-color:#096;color: #FFFFFF;text-align: center;font-size: 16px;padding: 10px;  width: auto;transition: all 0.5s;cursor: pointer;margin: 2px;}
 .button span {cursor: pointer;display: inline-block;position: relative;transition: 0.5s;}
 .button span:after {content: 'ADD';position: absolute;opacity: 0;top: 0;right: -20px; transition: 0.5s;}
@@ -38,7 +38,7 @@ cursor: pointer; margin: 2px;background-image:url(img/print-button.png); backgro
 .button1 span {cursor: pointer;display: inline-block;position: relative;transition: 0.5s;}
 .button:hover span:after { opacity: 1; right: 0;}
 .test:hover{ background-color:#F4F4F4;}
-input, select{border: 1px solid #069;  height:25px; padding-left:30px;  font-size:16px;}
+input, select{border: 1px solid #069;  height:27px; padding-left:30px;  font-size:16px;}
 </style>
 <script src="search/jquery.js" type="text/javascript"></script>
 <script>
