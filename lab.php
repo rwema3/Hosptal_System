022c5b0b7ef10be3719d8391884216eb013a4692
rwema3 <rwemadominik3@gmail.com>
2022-01-23 17:24:36 +0200

    HS update
    HS update

--- lab.php
+++ lab.php
@@ -23,7 +23,7 @@ mysqli_query($link,"CREATE VIEW labo AS SELECT act FROM acts WHERE type='LABORAT
 <title>Untitled Document</title>
 <style>
 body{width:610px;}
-.frmSearch {border: 1px solid #a8d4b1;background-color: #c6f7d0;margin: 2px 0px;padding:40px;border-radius:4px;}
+.frmSearch {border: 1.5px solid #a8d4b1;background-color: #c6f7d0;margin: 2px 0px;padding:40px;border-radius:4px;}
 #country-list{float:left;list-style:none;margin-top:-3px;padding:0;width:485px;position: absolute;}
 #country-list li{padding: 10px; background: #f0f0f0; border-bottom: #bbb9b9 1px solid;}
 #country-list li:hover{background:#ece3d2;cursor: pointer;}
